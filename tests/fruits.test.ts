c2620d8cebb7d3aaf54aa597e69938c9229bfe95
gabriel <gabriel.pdg8@gmail.com>
2023-04-20 16:48:49 -0300

    finalizado

---
+++ tests/fruits.test.ts
@@ -0,0 +1,60 @@
+import supertest from "supertest";
+import app from "../src/index"
+
+const api = supertest(app);
+
+describe("/fruits", () => {
+    it("POST /fruits should respond with code 201", async () => {
+        const fruits = {
+            name: "Uva",
+            price: 10,
+        };
+        const result = await api.post("/fruits").send(fruits);
+        expect(result.status).toBe(201);
+    });
+
+    it("POST /fruits should respond with code 409", async () => {
+        const fruits = {
+            name: "Uva",
+            price: 10,
+        };
+        const result = await api.post("/fruits").send(fruits);
+        expect(result.status).toBe(409);
+    });
+
+    describe("GET /fruits", () => {
+        it("should get all fruits", async () => {
+    
+            const result = await api.get('/fruits');
+            expect(result.status).toBe(200)
+            expect(result.body).toEqual([
+                {
+                    id: expect.any(Number),
+                    name: expect.any(String),
+                    price: expect.any(Number), 
+                }
+            ])
+        })
+    })
+    
+    describe('GET /fruits/:id', () => {
+        it('should respond with status 404', async () => {
+            const result = await api.get('/fruits/3');
+
+            expect(result.status).toBe(404);
+        })
+
+        it('should respond with status 200', async () => {
+            const body = {
+                "name": "Uva",
+                "price": 10
+            };
+
+            await api.post('/fruits').send(body);
+            const result = await api.get('/fruits/1');
+
+            expect(result.body.id).toEqual(1);
+            expect(result.status).toBe(200)
+        })
+    })
+})
